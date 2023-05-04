347da7044e13cbec416c4d583ce0dcc033d71e32
jdelmazo9 <j.delmazo@hotmail.com>
2023-05-04 15:34:15 -0300

    handle overlay click with css selector

--- components/AccesibleModal.tsx
+++ components/AccesibleModal.tsx
@@ -1,40 +1,25 @@
-import { FC, useEffect, useRef, useState } from "react"
+import { FC, useState } from "react"
+import ReactDOM from "react-dom";
 
 const AccessibleModal: FC = () => {
   const [isVisible, setIsVisible] = useState(false);
-  const ref = useRef<HTMLDivElement | null>(null);
 
-  const assertIsNode: (e: EventTarget | null) => asserts e is Node = (e) => {
-    if (!e || !("nodeType" in e)) {
-      throw new Error(`Node expected`);
-    }
-  }
-
-  useEffect(() => {
-    const listener = ({ target }: MouseEvent): void => {
-      assertIsNode(target);
-      if (!ref.current || ref.current.contains(target as Node)) {
-        return;
-      }
+  const handleOverlayClick = ({ target }: React.MouseEvent): void => {
+    if (!(target as HTMLDivElement).matches('[aria-modal="true"] *')) {
       setIsVisible(false);
-    };
-    document.addEventListener('mousedown', listener);
-    return () => {
-      document.removeEventListener('mousedown', listener);
-    };
-  },
-    [ref.current],
-  );
+    }
+  };
 
   return (
     <div>
-      {isVisible &&
-        <div className="fixed inset-0 flex items-center justify-center bg-black/30" aria-modal='true'>
-          <div className="bg-white p-5 rounded-lg shadow-lg" role="dialog" ref={ref}>I am an accessible modal! 🚀 If you want to close me just click around</div>
-        </div>}
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setIsVisible(true)}>
-        Open modal
-      </button>
+      {isVisible && ReactDOM.createPortal(
+        <div className="fixed inset-0 flex items-center justify-center bg-black/30" aria-modal='true' onClick={handleOverlayClick}>
+          <div className="bg-white p-5 rounded-lg shadow-lg" role="dialog">I am an accessible modal! 🚀 If you want to close me just click around</div>
+        </div>
+        , document.body)
+      }
+      <button className="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => setIsVisible(true)}>Open modal</button>
+
     </div >
   );
 };
